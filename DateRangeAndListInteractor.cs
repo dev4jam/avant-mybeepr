87b38a20a2898ec68a6c4c191431ad05ba74b720
Dmitry Klimkin <dmitry.klimkin@westpac.com.au>
2018-11-29 14:10:30 +1100

    Initial files

---
+++ DateRangeAndListInteractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DateExtension;
+
+namespace BizDays1 {
+    public class DateRangeAndListInteractor {
+        public int GetBizDays(DateTime startDate, DateTime endDate, DateTime[] holidays) {
+            var interactor = new DateRangeInteractor();
+            var totalBizDays = interactor.GetBizDays(startDate, endDate);
+
+            foreach (DateTime holiday in holidays) {
+                if (holiday.InRange(startDate, endDate) && holiday.IsWeekday()) {
+                    totalBizDays--;
+                }
+            }
+
+            return totalBizDays;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace BizDays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interactor = new DateRangeInteractor();
+            var startDate = new DateTime(2018, 11, 23);
+            var endDate = new DateTime(2018, 11, 26);
+
+            var bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 0");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 24);
+            endDate = new DateTime(2018, 11, 26);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 0");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 25);
+            endDate = new DateTime(2018, 11, 26);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 0");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 26);
+            endDate = new DateTime(2018, 11, 27);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 0");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 26);
+            endDate = new DateTime(2018, 11, 28);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 1");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 22);
+            endDate = new DateTime(2018, 11, 24);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 1");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 22);
+            endDate = new DateTime(2018, 11, 25);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 1");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 21);
+            endDate = new DateTime(2018, 11, 25);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 2");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 15);
+            endDate = new DateTime(2018, 11, 27);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 7");
+            Console.WriteLine(bizDays);
+
+            startDate = new DateTime(2018, 11, 15);
+            endDate = new DateTime(2018, 11, 26);
+
+            bizDays = interactor.GetBizDays(startDate, endDate);
+
+            Console.WriteLine("Should be: 6");
+            Console.WriteLine(bizDays);
+
+            var dynamicInteractor = new DynamicWorkDaysInteractor();
+            var holiday = new Holiday(DayOfWeek.Monday, 1, 6, true);
+            var holidays = new Holiday[] { holiday };
+
+            startDate = new DateTime(2018, 6, 8);
+            endDate = new DateTime(2018, 6, 15);
+
+            bizDays = dynamicInteractor.GetBizDays(startDate, endDate, holidays);
+
+            Console.WriteLine("Should be: 3");
+            Console.WriteLine(bizDays);
+        }
+    }
+}
